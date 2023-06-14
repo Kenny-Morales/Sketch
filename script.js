207098634f47fbcacb12024b1fb05e66e3698d03
Kenny Morales <kennymorales580@gmail.com>
2023-06-14 16:04:36 -0500

    created function to determine the size of the divs

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const canvasLength = 1000;
+
 function userGridLength() {
     gridLength = prompt("Type in a grid size");
     return gridLength;
 }
+
+function sizeOfDivs() {
+    divSize = canvasLength / gridLength;
+    return divSize;
+}
+
+
+
