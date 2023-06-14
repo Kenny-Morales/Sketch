81e42354cab58d0027abbd71ff8333726af181c5
Kenny Morales <kennymorales580@gmail.com>
2023-06-14 17:00:08 -0500

    cleaned the javascript code into one function

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
 const canvasLength = 1000;
 
-function userGridLength() {
-    gridLength = prompt("Type in grid size (Ex: 16 => 16 x 16)");
-    return gridLength;
-}
-
-function sizeOfDivs() {
-    gridLength = userGridLength();
-    divSize = canvasLength / gridLength;
-    return divSize;
-}
-
-function numOfDivs() {
-    gridLength = userGridLength();
-    divNum = Math.pow(gridLength, 2);
-    return (divNum);
-}
-
 function gridGenerator() {
-    divNum = numOfDivs();
-    divSize = sizeOfDivs();
+    var gridLength = prompt("Type in grid size (Ex: 16 => 16 x 16)");
+    var divNum = Math.pow(gridLength, 2);
+    var divSize = canvasLength / gridLength;
     for (i = 0; i < divNum; i++) {
-    var div = document.createElement("div");
-    div.style.width = divSize + "px";
-    div.style.height = divSize + "px";
-    document.getElementById("grid").appendChild(div);
-    };
+        var div = document.createElement('div');
+        div.style.width = divSize + "px";
+        div.style.height = divSize + "px";
+        div.style.backgroundColor = 'blue';
+        document.getElementById("grid").appendChild(div);
+    }
 }
 
