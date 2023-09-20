f0353541ec577396342f71be17414678d9b90f8b
Kenny Morales <kenny-morales@outlook.com>
2023-09-20 03:11:05 -0500

    Finished changes and cleaned up UI

--- script.js
+++ script.js
@@ -2,22 +2,29 @@ const canvasLength = 500;
 
 function gridGenerator() {
     const button = document.getElementById("button");
-    button.remove();
+    clearGrid(); // Clear the grid before generating a new one
     var gridLength = prompt("Type in grid size (Ex: 16 => 16 x 16)");
     var divNum = Math.pow(gridLength, 2);
     var divSize = canvasLength / gridLength;
-    for (i = 0; i < divNum; i++) {
+    for (let i = 0; i < divNum; i++) {
         var div = document.createElement('div');
         div.classList.add("child");
         div.style.width = divSize + "px";
         div.style.height = divSize + "px";
         div.style.backgroundColor = 'white';
-        
-        div.addEventListener("mouseover", function() {
-                this.style.backgroundColor = "black";
+
+        div.addEventListener("mouseover", function () {
+            this.style.backgroundColor = "black";
         });
-        
+
         document.getElementById("grid").appendChild(div);
+    }
+}
+
+function clearGrid() {
+    const grid = document.getElementById("grid");
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
     }
 }
 
